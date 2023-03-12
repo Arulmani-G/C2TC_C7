de5e5f5be48b0fc8c017f6a854809a282063db40
ELCOT <ELCOT@Arulmani>
2023-03-12 21:53:18 +0530

    hi

---
+++ Day_3/src/ClassesAndObjects/FootBallPlayer.java
@@ -0,0 +1,13 @@
+package ClassesAndObjects;
+
+public class FootBallPlayer {
+
+	public static void main(String[] args) {
+		Player ply = new Player();
+		ply.name="Ronaldo";
+		ply.playerNo=7;
+		ply.scores=350;
+		ply.display();
+	}
+}
+
---
+++ Day_3/src/ClassesAndObjects/Player.java
@@ -0,0 +1,11 @@
+package ClassesAndObjects;
+
+public class Player {
+	String name;
+	int playerNo;
+	int scores;
+	void display() {
+		System.out.println("Name: "+name+"\n"+"Player No: "+playerNo+"\n"+"Scores: "+scores);
+	}
+	
+}
